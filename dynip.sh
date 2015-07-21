e6d876200cc85dc5c940bd71aa41d1b96466234c
KayDat <kitat.chung@gmail.com>
2015-07-21 20:48:13 +1000

    dig instead of curl
    Use dig and OpenDNS instead of curl and iroffer's cgi script. Runs much much faster, less load on iroffer's server and more reliable as it uses the DNS protocol instead of HTTP. OpenDNS can be substituted with any other public DNS service.
    
    Since this checks for changes via DNS, the script can be run via cron every minute instead of every five.
    
    Idea taken directly from StackExchange thread: http://unix.stackexchange.com/a/81699

--- dynip.sh
+++ dynip.sh
@@ -2,27 +2,26 @@
 
 #### WHAT THIS DOES ####
 # 
-# If you have a dynamic ip address and require setting 'usenatip'
-# in the iroffer config file this script can auto-generate a 1 line
-# config file containing your current ip address
-# run iroffer with multiple config files (your normal config plus
-# this auto-generated config)
-# 
-# If your ip address changes, the file will be updated by this script
-# and iroffer rehashed to see the change
+# If you have a dynamic IP address and require the setting 'usenatip' in
+# the iroffer config file, then this script can auto-generate a one-line
+# config file containing your current IP address. Run iroffer with
+# multiple config files (your normal config plus this auto-generated
+# config).
 #
-# Call this scrip when your IP changes or put in in your crontab.
-# Rin "crontab -e" and place the following line in the editor:
-# */5 * * * * /full/path/to/iroffer/dynip.sh
+# If your IP address changes, the config file will be updated by this
+# script and iroffer rehashed to see the change.
+#
+# Call this script when your IP changes or put in in your crontab.
+# Run "crontab -e" and place the following line in the editor:
+# * * * * * /full/path/to/iroffer/dynip.sh
 
-URL="http://iroffer.org/myip.cgi"
 CONFIGFILE="dynip.config"
 PIDFILE="mybot.pid"
 
 set -e
 set -u
 
-curl --silent ${URL} \
+dig +short myip.opendns.com @resolver1.opendns.com \
  |sed -e 's=^=usenatip =' \
  > ${CONFIGFILE}.tmp
 
